141468f2541e81b113e8463706645ab46653617a
Naman Oli <namanoli111@gmail.com>
2021-08-02 09:32:38 +0530

    Updates

---
+++ precache-manifest.04b333a9967d521951ab9bed433c3abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0832f74791764cfade99b6f30004a32c",
+    "url": "/MovFlix/index.html"
+  },
+  {
+    "revision": "e901a0f9c91612df7cbe",
+    "url": "/MovFlix/static/css/main.2e5ac4c4.chunk.css"
+  },
+  {
+    "revision": "20b1c23231ff1bfb8a61",
+    "url": "/MovFlix/static/js/2.cf1e881b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovFlix/static/js/2.cf1e881b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e901a0f9c91612df7cbe",
+    "url": "/MovFlix/static/js/main.a63ca202.chunk.js"
+  },
+  {
+    "revision": "2d9b524fb07970c929e5",
+    "url": "/MovFlix/static/js/runtime-main.9362a5fe.js"
+  }
+]);
